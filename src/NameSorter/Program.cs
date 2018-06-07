d066e3d9a5deba83c3375750ca4c4969d75d702d
Yue Zhou <yue@hemamaps.com.au>
2018-06-07 14:38:26 +1000

    temporary commit

--- src/NameSorter/Program.cs
+++ src/NameSorter/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace NameSorter
+﻿namespace NameSorter
 {
+    using Microsoft.Extensions;
+    using Microsoft.Extensions.CommandLineUtils;
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new CommandLineApplication();
+            app.Name = "name-sorter";
+            app.Description = "Help sort names by last name then given name.";
+            app.HelpOption("-?|-h|--help");
+
+            var fileOption = app.Option("-f|--file <file>", "names in text file for sort", CommandOptionType.SingleValue);
+
+            app.OnExecute(() => {
+                var file = fileOption.Value();
+                Console.Write($"Reading file... {file}");
+                return 0;
+            });
+
+            app.Execute(args);
         }
     }
 }
